25e1572c6d284d244088b83de07079f8d510d17c
Geoffrey <c25geoffrey.stentiford@afacademy.af.edu>
2025-04-18 14:18:10 -0600

    Revert "98% solution"
    This reverts commit 58d0649e20a65f02aaf6c0d2e524b5763d595d08.

--- transcription/speech_recognition.py
+++ transcription/speech_recognition.py
@@ -5,11 +5,6 @@ import sys
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, Pipeline, pipeline
 from typing import Dict
-from ollama import Client
-from pydantic import BaseModel
-import requests
-
-LLM_MODEL: str = "gemma3:27b"  # Change this to be the model you want
 
 def record_audio(duration_seconds: float = 0.75) -> npt.NDArray[np.float32]:
     """Record duration_seconds of audio from default microphone.
@@ -43,66 +38,11 @@ def build_pipeline(model_id: str, torch_dtype: torch.dtype, device: str) -> Pipe
         device=device,
     )
     return pipe
-    
-client: Client = Client(
-    host="http://ai.dfec.xyz:11434"  # Change this to be the URL of your LLM
-)
-
-class Place(BaseModel):
-    place: str
-    point: bool
-    error: str
-
-def llm_parse_for_wttr(prompt: str):
-    response = client.chat(
-        messages=[
-            {
-                "role": "system",
-                "content": '''
-                Your purpose is to extract the intended location from the user's weather request.
-                The request is transcribed from speech by a different AI model and may contain errors.
-                Take your time to ensure correct response. Place the extracted location into `place` as a string.
-                
-                The `point` field of the JSON output specifies if the location is a specific point, such as airports,
-                train stations, landmarks, named spots, or anything else along those lines.
-                In that case, set `point` to true.
-                
-                Places which are NOT points are things like cities, counties, states, countries, provinces, regions, military bases,
-                departments, cantons, prefectures, arrondissements, boroughs, communes, districts, towns, 
-                Census-Designated Places in the U.S., villages, tribal reservations, neighborhoods. In any of these
-                cases or similar, set `point` to false.
-                
-                Ensure the location is in a concise format, such as:
-                "Paris", "Denver, Colorado", "Taoyuan, Taiwan", "New York City", "Osaka International Airport" or "Tokyo Skytree".
-                
-                If the given location appears to be a three-letter airport code, leave it intact.
-                Examples of airport codes include: "FUK", "MUC", "BKK", "SFO", and "KHH".
-                
-                The transcription model may add extraneous punctuation into spoken airport codes. For instance, 
-                "BKK" is transcribed as "BK.K." Look for occurences of such errors and correct them.               
-                
-                If for whatever reason you are unable to comply with instructions above, explain
-                yourself in a brief manner using `error`. Otherwise, set `error` to a blank string.
-                '''
-            },
-            {"role": "user", "content": prompt},
-        ],
-        model=LLM_MODEL,
-        format=Place.model_json_schema()
-    )        
-    place = Place.model_validate_json(response.message.content)
-    
-    # if len(place.error) > 0:
-    print(place.error)
-    
-    if place.point and not (place.place.upper() == place.place and len(place.place) == 3):
-        place.place = "~" + place.place
-    
-    return place.place.replace(" ", "+")
 
 if __name__ == "__main__":
-    model_id = "distil-whisper/distil-medium.en"
-    print("Using model_id " + model_id)
+    # Get model as argument, default to "distil-whisper/distil-medium.en" if not given
+    model_id = sys.argv[1] if len(sys.argv) > 1 else "distil-whisper/distil-medium.en"
+    print("Using model_id {model_id}")
     # Use GPU if available
     device = "cuda" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
@@ -110,7 +50,8 @@ if __name__ == "__main__":
 
     print("Building model pipeline...")
     pipe = build_pipeline(model_id, torch_dtype, device)
-    print("Ready")
+    print(type(pipe))
+    print("Done")
 
     while True:
         #print("Recording")
@@ -121,9 +62,4 @@ if __name__ == "__main__":
         if "computer" in speech["text"].lower():
                 print("Recording prompt")
                 speech = pipe(record_audio(5))
-                location = llm_parse_for_wttr(speech["text"])
-                try:
-                    wttr = requests.get("https://wttr.in/" + location)
-                    print(wttr.text)
-                except:
-                    print("There was an error in retrieving weather data.")
+                print(speech["text"])
